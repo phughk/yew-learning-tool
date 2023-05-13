73e7001ac8321c38cb542b904e63621bd2da1aa8
Hugh Kaznowski <phkaznowski@gmail.com>
2023-05-13 15:24:50 +0100

    Add tailwind theme and configure Dataset component

--- src/dataset.rs
+++ src/dataset.rs
@@ -3,9 +3,18 @@ use yew::prelude::*;
 #[function_component(Dataset)]
 pub fn dataset() -> Html {
     html! {
-        <div class={classes!("dataset", "bg-red-100")}>
-            <p class={classes!("bg-red-100")}>{"Test!"}</p>
-            <p>{"This is a single dataset"}</p>
-        </div>
-    }
+        <div class={classes!("bg-white", "rounded-lg", "shadow-md", "p-4")}>
+  <h2 class={classes!("text-2xl", "font-bold", "text-accent", "mb-2")}>{"Dataset Name"}</h2>
+  <p class={classes!("text-gray-600", "mb-4")}>{"Tags: Tag1, Tag2, Tag3"}</p>
+  <p class={classes!("text-gray-600", "mb-4")}>{"Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit."}</p>
+  <div class={classes!("flex", "items-center", "justify-between")}>
+    <div class={classes!("text-gray-500")}>{"Author: John Doe"}</div>
+    <div class={classes!("text-right")}>
+      <span class={classes!("text-gray-500")}>{"Last Used: 2023-05-13 09:30 AM"}</span>
+      <span class={classes!("ml-2", "text-gray-500")}>{"Progress: 80%"}</span>
+    </div>
+  </div>
+</div>
+
+}
 }
