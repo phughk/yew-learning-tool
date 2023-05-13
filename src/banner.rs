2e5d0cb01f9772a23894aa7850ba11f3c2163a38
Hugh Kaznowski <phkaznowski@gmail.com>
2023-05-13 13:56:56 +0100

    Fixup dataset page

--- src/banner.rs
+++ src/banner.rs
@@ -5,7 +5,7 @@ pub fn banner() -> Html {
     html! {
         <div class="banner">
             <a href="/profile">{"Profile"}</a>
-            <a href="/datasets">{"Datasets"}</a>
+            <a href="/dataset">{"Datasets"}</a>
             <a href="/about">{"About"}</a>
             <a href="/repository">{"Repository"}</a>
         </div>
