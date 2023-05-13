4cc282f7795148d919a4bd5a4788eec78f46c4d1
Hugh Kaznowski <phkaznowski@gmail.com>
2023-05-13 14:45:28 +0100

    Improve about

--- src/about.rs
+++ src/about.rs
@@ -4,7 +4,22 @@ use yew::prelude::*;
 pub fn about() -> Html {
     html! {
         <div class="about">
-            <p>{"about page"}</p>
+            <p>{"This tool is used to learn by quizzing"}</p>
+            <p>
+                {"The frontend is written in Rust WebAssembly using"}
+                <a href={"https://yew.rs/"}>{"yew"}</a>{"."}
+            </p>
+            <p>{"The purpose of the project is to learn things quickly that are otherwise tedious to learn."}</p>
+            <p>{"For example"}</p>
+            <ul>
+                <li>{"languages"}</li>
+                <li>{"concepts (from programming or other)"}</li>
+                <li>{"statistics and other constants"}</li>
+                <li>{"properties (such as Keplar's laws)"}</li>
+                <li>{"patterns - design patterns or other (ways of writing code, ways of structuring data, ways of writing a document)"}</li>
+                <li>{"geography"}</li>
+                <li>{"... literally anything that is a boring dataset that is nice to memorise"}</li>
+            </ul>
         </div>
     }
 }
