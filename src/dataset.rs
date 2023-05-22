1c2503fa9ef66d3d6c09aa2739200d8448646efd
Hugh Kaznowski <phkaznowski@gmail.com>
2023-05-23 00:55:27 +0100

    Add props to Dataset

--- src/dataset.rs
+++ src/dataset.rs
@@ -1,17 +1,39 @@
 use yew::prelude::*;
 
+use yew::{function_component, html, Html, Properties};
+
+#[derive(Properties, PartialEq, Default)]
+pub struct Props {
+    #[prop_or_default]
+    pub name: AttrValue,
+    #[prop_or_default]
+    pub tags: Vec<AttrValue>,
+    #[prop_or_default]
+    pub description: AttrValue,
+    #[prop_or_default]
+    pub author: AttrValue,
+    #[prop_or_default]
+    pub last_used: AttrValue,
+    #[prop_or_default]
+    pub progress: AttrValue,
+}
+
 #[function_component(Dataset)]
-pub fn dataset() -> Html {
+pub fn dataset(props: &Props) -> Html {
+    let shadow = match props.name.as_str() {
+        "two" => "shadow-lg",
+        _ => "shadow-sm"
+    };
     html! {
-        <div class={classes!("bg-white", "rounded-lg", "shadow-md", "p-4")}>
-          <h2 class={classes!("text-2xl", "font-bold", "text-accent", "mb-2")}>{"Dataset Name"}</h2>
-          <p class={classes!("text-gray-600", "mb-4")}>{"Tags: Tag1, Tag2, Tag3"}</p>
-          <p class={classes!("text-gray-600", "mb-4")}>{"Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit."}</p>
+        <div class={classes!("bg-white", "rounded-lg", {{shadow}}, "p-4")}>
+          <h2 class={classes!("text-2xl", "font-bold", "text-accent", "mb-2")}>{&props.name}</h2>
+          <p class={classes!("text-gray-600", "mb-4")}>{"Tags: "}{for props.tags.iter()}</p>
+          <p class={classes!("text-gray-600", "mb-4")}>{"Description: "}{&props.description}</p>
           <div class={classes!("flex", "items-center", "justify-between")}>
-            <div class={classes!("text-gray-500")}>{"Author: John Doe"}</div>
+            <div class={classes!("text-gray-500")}>{"Author: "}{&props.author}</div>
             <div class={classes!("text-right")}>
-              <span class={classes!("text-gray-500")}>{"Last Used: 2023-05-13 09:30 AM"}</span>
-              <span class={classes!("ml-2", "text-gray-500")}>{"Progress: 80%"}</span>
+              <span class={classes!("text-gray-500")}>{"Last Used: "}{&props.last_used}</span>
+              <span class={classes!("ml-2", "text-gray-500")}>{"Progress: "}{&props.progress}</span>
             </div>
           </div>
         </div>
--- src/datasets.rs
+++ src/datasets.rs
@@ -1,20 +1,23 @@
 use yew::prelude::*;
-use crate::dataset::Dataset;
+use yew::props;
+use yew::virtual_dom::VNode;
+use crate::dataset::{Dataset, Props};
 
 #[function_component(Datasets)]
 pub fn datasets() -> Html {
-    let items = vec![
-        "one",
-        "two",
-        "three"
+    let props = vec![
+        props!(Props{name: "First", tags: vec!["one".into(), "two".into()]}),
+        props!(Props{name: "Second", tags: vec!["three".into(), "four".into()]}),
+        props!(Props{name: "Third", tags: vec!["five".into()]}),
     ];
     html! {
         <div class="datasets">
             <p>{"Datasets page"}</p>
             <ul>
-                { items.iter().map(|props| {
-                    html!{<Dataset/>}
-                }).collect::<Html>() }
+                {props.into_iter().map(|props| {
+                    html!{ <li><Dataset ..props /></li>}
+                }).collect::<Html>()
+                }
             </ul>
         </div>
     }
