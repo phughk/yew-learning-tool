c76053215a862e4eb9e8379af12cc04081e38ac3
Hugh Kaznowski <phkaznowski@gmail.com>
2023-07-08 18:17:21 +0100

    Add description to repository component

--- src/repository.rs
+++ src/repository.rs
@@ -5,6 +5,8 @@ pub fn repository() -> Html {
     html! {
         <div class="repository">
             <p>{"Repository page"}</p>
+            <p>{"The purpose of the repository page is to point to servers that host learning files."}</p>
+            <p>{"These files can then be downloaded to the local host."}</p>
         </div>
     }
 }
